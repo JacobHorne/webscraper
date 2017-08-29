41ff0c8f8a9625dce7b2a11ff175d0724b08f3cd
Jake <jacobhorne@gmail.com>
2017-08-29 16:58:28 -0400

    f

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,7 @@
 <template>
  
   <div class="container">
-    <div class="row" style="margin:0 auto; max-width:1000px;">
+    <div class="row">
       <div class="column column-40">
         <label for="nameField">WEBSITE</label>
         <input type="text" v-model="website" placeholder="medium.com" id="nameField">
@@ -11,14 +11,14 @@
         <input type="text" v-model="tag" placeholder="h1,p,li" id="nameField">
       </div>
       <div class="column column-40 float-left" style="margin-top:1.8em" >
-        <button class="button-large">Scrape</button>
-        <button class="button-large button-outline">Reset</button>
+        <button class="button-large" @click="alertMe()">Scrape</button>
+        <!--<button class="button-large button-outline">Reset</button>-->
       </div>
     </div>
     
     <div class="row">
       <div class="column">
-        
+        <p v-for="dog in data">{{dog}}</p>
       </div>
     </div>
   </div>
@@ -26,12 +26,30 @@
 </template>
 
 <script>
+
+import axios from 'axios'
+
 export default {
   name: 'hello',
   data () {
     return {
       website: '',
-      tag
+      tag: '',
+      data: null
+    }
+  },
+  methods : {
+    alertMe(){
+      let self = this;
+      axios.get('https://dog.ceo/api/breeds/list/all')
+      .then(function (response) {
+        console.log(response.data.message)
+        self.data = response.data.message;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
     }
   }
 }
