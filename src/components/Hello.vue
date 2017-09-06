f4c409797d793cb69faa56e1c7d5df3aa8b41946
Jake <jacobhorne@gmail.com>
2017-09-06 17:12:42 -0400

    boostrap + custom input

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,41 +1,43 @@
 <template>
  
-  <div class="container">
-    <div class="row">
-
-      <!--user input-->
-      <div class="column">
-        <label for="nameField">WEBSITE</label>
-        <input type="text" v-model="url" placeholder="medium.com" id="nameField">
-      </div>
-
-      <div class="column">
-        <label for="tags">HTML TAG</label>
-        <select id="tags" v-model="tag">
-          <option value="h1">H1</option>
-          <option value="h2">H2</option>
-          <option value="h3">H3</option>
-          <option value="p">p</option>
-          <option value="li">li</option>
-          <option value="td">td</option>
-        </select>
-        <span>Selected: {{ tag }}</span>
-      </div>
-      
-      <div class="column" style="margin-top:1.8em" >
-        <button class="button-large" @click="scrape()">Scrape</button>
-        <!--<button class="button-large button-outline">Reset</button>-->
-      </div>
+  <div class="container center-block">
+    <div class="row" align="center">
+      <!--web input-->
+      <div class="col-xs-8" >
+        <div class="input-group input-group-lg">
+          <span class="input-group-addon" id="prefix">https://</span>
+          <input type="text" v-model="url" class="form-control" placeholder="Medium.com" aria-describedby="basic-addon3">
+        </div>
+      </div>    
     </div>
     
+    <!--tag input-->
+    <div class="row" >
+      <div class="col-xs-12" style="margin:2em 0">
+        <div class="col-xs-8">
+          <input v-model="userInput" class="form-control"/>
+        </div>
+         <div class="col-xs-4">
+        <button class="btn-primary" @click="add(userInput)">Add Tag</button>
+        </div>
+        <div style="margin:1em 0;">
+          <span class="chip" v-for="(tag, index) in tags" :key="tag" id="tag"> {{tags[index]}} <span @click="remove(tag)"> X </span> </span>
+        </div>
+      </div>
+    </div>  
+      
+      <div class="column" style="margin-top:3em" >
+        <button class="btn btn-lg" @click="scrape()">Scrape</button>
+        <!--<button class="button-large button-outline">Reset</button>-->
+      </div>
+   
     <div class="row">
       <div class="column">
         <h3>Results:</h3>
         {{data}}
       </div>
     </div>
-  </div>
-
+ </div>
 </template>
 
 <script>
@@ -47,21 +49,31 @@ export default {
   data () {
     return {
       url: '',
-      tag: '',
-      data: null
+      tags: [],
+      data: null,
+      userInput:null
     }
   },
   methods : {
     scrape(url, tag){
       let self = this;
-      axios.get(`http://localhost:3000/scrape?url=http://${this.url}&tag=${this.tag}`)
+      // axios.get(`http://localhost:3000/scrape?url=http://${this.url}&tag=${this.tag}`)
+      axios.post(`http://localhost:3000/scrape`, this.tags)
         .then( (response) => {
           self.data = response.data;
         })
         .catch( (error) => {
           console.log(error);
-          self.data = error;
+          self.data = 'There was an error'
         });
+    },
+    add(userInput){
+      if(userInput !== null){
+        this.tags.push(userInput);
+      }
+    },
+    remove(index){
+      this.tags.pop(index);
     }
   }
 }
@@ -77,47 +89,24 @@ ul {
   list-style-type: none;
   padding: 0;
 }
-
+#prefix{
+  color:grey;
+  background:lightgrey;
+  border:none;
+}
 li {
   display: inline-block;
   margin: 0 10px;
 }
-
+#tag{
+  background:1em;
+  color:grey;
+  border:1px solid grey;
+  border-radius:5px;
+  padding:.3em 2em;
+  margin:0 1em;
+}
 a {
   color: #42b983;
-}
-input{
-  padding: 2.1rem 1.0rem;
-  font-size:1em;
-  display: inline-block;
-  transition:ease all .5s;
-    &:focus{
-    opacity:1;
-    transform:scale(1.1,1.1);
-  }
-}
-select{
-  transition:ease all .5s;
-  &:focus{
-    opacity:1;
-    transform:scale(1.1,1.1);
-  }
-}
-#tags{
-  padding: 2.1rem 1.0rem;
-}
-.button-large {
-  font-size: 1.4rem;
-  height: 4.5rem;
-  line-height: 4.0rem;
-  padding: 0 4rem;
-  background-color:#2f2f2f;
-  border:2px solid #2f2f2f;
-  transition:ease all .5s;
-  opacity:.80;
-  &:hover{
-    opacity:1;
-    transform:scale(1.1,1.1);
-  }
 }
 </style>
